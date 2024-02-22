343ce842d0eea5669a3c5fa46501062fb0138056
Norman Pong <zhiming3399@gmail.com>
2024-02-22 16:19:08 +0800

    feat(provide): example for "useClass useValue useFactory useExisting"

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,9 +1,27 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Inject } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(
+    private readonly appService: AppService,
+
+    @Inject('custom_object')
+    private readonly custom_object: {
+      type: 'object';
+      name: 'norman';
+    },
+
+    @Inject('custom_factory')
+    private readonly custom_factory: {
+      type: 'factory_object';
+      name: string;
+      age: number;
+    },
+
+    @Inject('custom_object_existing')
+    private readonly custom_object_existing: typeof custom_object,
+  ) {}
 
   @Get()
   getHello(): string {
--- src/app.module.ts
+++ src/app.module.ts
@@ -6,6 +6,32 @@ import { PersonModule } from './person/person.module';
 @Module({
   imports: [PersonModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    {
+      provide: AppService,
+      useClass: AppService,
+    },
+    {
+      provide: 'custom_object',
+      useValue: {
+        type: 'object',
+        name: 'norman',
+      },
+    },
+    {
+      provide: 'custom_factory',
+      useFactory() {
+        return {
+          type: 'factory_object',
+          name: 'norman',
+          age: 18,
+        };
+      },
+    },
+    {
+      provide: 'custom_object_existing',
+      useExisting: 'custom_object',
+    },
+  ],
 })
 export class AppModule {}
